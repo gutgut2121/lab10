1052e4e6168bea4983b9bbc5d2159e65a9d786d4
gutgut2121 <2121haisakisami@gmail.com>
2021-04-27 16:58:05 +0800

    some

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apitext: '' };
+    }
+    componentDidMount() {
+    fetch("https://u2myxspje6.execute-api.us-east-1.amazonaws.com/default/hello-world")
+    .then(res => res.json())
+    .then(
+    (result) => {
+    this.setState({ apitext: result.text + result.time });
+    });
+    }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Hello Edit <code>src/App.js</code> and save to reload.
+        {this.state.apitext}
         </p>
         <a
           className="App-link"
@@ -20,6 +34,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
